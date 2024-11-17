37e1622340ee2c3a27553515fa8625a82f70d527
jbutterworthUA <148112741+jbutterworthUA@users.noreply.github.com>
2024-11-17 16:50:34 -0700

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,3 +1,15 @@
+/*
+ * enums for types of scoring categories.
+ *
+ * authors:
+ * @ Garret W
+ * @ JOhn I
+ * @ Dylan C
+ * @ Jason B
+ */
 public enum Category {
-  
+  ONES, TWOS, THREES, FOURS, FIVES, SIXES,
+  THREEOFKIND, FOUROFKIND, FULLHOUSE,
+  SMALLSTRAIGHT, LARGESTRAIGHT,
+  YAHTZEE, CHANCE;
 }
