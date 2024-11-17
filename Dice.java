85cf1ca13e0aeee183fa364d39e8c1cebc03da8d
gsw2019 <wsg@arizona.edu>
2024-11-17 16:38:31 -0700

    worked on DiceEnum class

--- Dice.java
+++ Dice.java
@@ -1,3 +1,4 @@
 public class Dice {
     // testing adding a file
+
 }
---
+++ DiceEnum.java
@@ -0,0 +1,31 @@
+/*
+ * enums for the Dice objects
+ *
+ * authors:
+ * @ Garret W
+ * @ JOhn I
+ * @ Dylan C
+ * @ Jason B
+ */
+
+
+public enum DiceEnum {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6);
+
+    private final int value;
+
+    // constructor at runtime
+    DiceEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
