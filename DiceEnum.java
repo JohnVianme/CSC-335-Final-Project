fec19deb3b9230f17728919f47b12835cf4709dd
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2024-11-17 16:56:39 -0700

    adde a gitDice method

--- DiceEnum.java
+++ DiceEnum.java
@@ -10,21 +10,34 @@
  */
 
 public enum DiceEnum {
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6);
+	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6);
 
-    private final int value;
+	private final int value;
 
-    // constructor at runtime
-    DiceEnum(int value) {
-        this.value = value;
-    }
+	// constructor at runtime
+	DiceEnum(int value) {
+		this.value = value;
+	}
 
-    public int getValue() {
-        return value;
-    }
+	public int getValue() {
+		return value;
+	}
+
+	/*
+	 * Getter for dice object based on integer values
+	 *
+	 * @param val A integer for the Dice
+	 *
+	 * @return A Dice enum representing a Dice if valid ordinal
+	 * 
+	 * @return null if the value is not related to a Dice Enum
+	 */
+	public static DiceEnum getDice(int value) {
+		for (DiceEnum dice : DiceEnum.values()) {
+			if (dice.getValue() == value) {
+				return dice;
+			}
+		}
+		return null;
+	}
 }
