f86aec24c0fbf7ef3f8b641dad81278ac6c5ab44
jbutterworthUA <148112741+jbutterworthUA@users.noreply.github.com>
2024-11-17 16:44:08 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,7 @@
 public class Player {
-
+  private String name;
+  private int rollCount;
+  private Hand myHand;
+  private scoreCard myScoreCard;
+  private ArrayList<Category> availableCategories;
 }
