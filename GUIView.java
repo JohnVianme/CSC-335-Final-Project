9112ed2a5cfc822603893d8bfa8ed9ad0ea1f4e7
jbutterworthUA <148112741+jbutterworthUA@users.noreply.github.com>
2024-11-23 14:24:54 -0700

    Update GUIView.java

--- GUIView.java
+++ GUIView.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
 
@@ -8,6 +9,10 @@ public class GUIView extends JFrame {
     private static Game myGame = new Game();
     private JPanel panel;
     private JLabel instructionLabel;
+    private JLabel playerNumLabel;
+    private JLabel rollCountLabel;
+    private JLabel scoreLabel;
+    private JPanel dicePanel;
 
     public GUIView() {
         setUp();
@@ -18,8 +23,8 @@ public class GUIView extends JFrame {
         this.setSize(800, 800);
 
         // Adding welcome label for player's first time opening view.
-        instructionLabel = new JLabel("Welcome to Yahtzee! Select game mode below!");
-        instructionLabel.setBounds(200, 100, 600, 100);
+        instructionLabel = new JLabel("Welcome to Yahtzee! Select game mode below.");
+        instructionLabel.setBounds(250, 100, 600, 100);
         this.add(instructionLabel);
 
         // Adding the panel.
@@ -66,12 +71,93 @@ public class GUIView extends JFrame {
         panel.add(exitButton);
     }
 
+    /**
+     * Method to create the page where a player will play yahtzee during their turn.
+     */
+    private void playPage() {
+        // Wipe GUI to start from fresh slate.
+        this.getContentPane().removeAll();
+        this.setLayout(null);
+
+        // Will update playerNum label later, for now just declare its position.
+        playerNumLabel = new JLabel("Current player: ");
+        playerNumLabel.setBounds(50, 25, 200, 100);
+        this.add(playerNumLabel);
+
+        // Will update rollsRemaining count later, for now just declare its position.
+        rollCountLabel = new JLabel("Rolls remaining: ");
+        rollCountLabel.setBounds(650, 25, 200, 100);
+        this.add(rollCountLabel);
+
+        // Display the "roll" button, which will indicate we need a new set of dice displayed and decrement rollCount.
+        JButton rollButton = new JButton("Roll");
+        rollButton.setActionCommand("roll");
+        rollButton.addActionListener(new ButtonClickListener());
+        rollButton.setBounds(340, 600, 100, 50);
+        this.add(rollButton);
+
+        // Create a panel where the dice will be displayed. Do not need to fill it yet, since user has not rolled.
+        dicePanel = new JPanel();
+        dicePanel.setBounds(50, 350, 600, 200);
+        dicePanel.setLayout(new GridLayout(1, 5, 15, 0));
+        this.add(dicePanel);
+
+        this.revalidate();
+        this.repaint();
+
+    }
+
+    /**
+     * When user clicks roll button, display new dice and update rollCountLabel.
+     */
+    private void rollDice() {
+        // Get rid of existing dice. (If any).
+        dicePanel.removeAll();
+
+        // Retrieve five random dice from the model, and add each to the panel.
+        for (int i = 0; i < 5; i++) {
+            // For now, add one picture to make sure it is appearing in GUI.
+            // Update later with functionality.
+            
+        }
+
+    }
+
     // Click listener for the previously listed buttons.
     private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
             
-            // If statements to handle each possible command that was clicked.
+            // If statements to handle each game mode that is selected.
+
+            // Create game object with one player.
+            if (command.equals("cpu")) {
+                // Set up the page to prepare for the game.
+                playPage();
+            }
+            // Create game object with two players.
+            else if (command.equals("twoPlayer")) {
+                // Set up the page to prepare for the game.
+                playPage();
+            }
+            // Create game object with three players.
+            else if (command.equals("threePlayer")) {
+                // Set up the page to prepare for the game.
+                playPage();
+            }
+            // Create game object with four players.
+            else if (command.equals("fourPlayer")) {
+                // Set up the page to prepare for the game.
+                playPage();
+            }
+            // Display a new set of dice for the player and update their available rollCount.
+            else if (command.equals("roll")) {
+                rollDice();
+            }
+            // Close the program.
+            else if (command.equals("exit")) {
+                System.exit(0);
+            }
         }
     }
 
@@ -81,3 +167,4 @@ public class GUIView extends JFrame {
         view.setVisible(true);
     }
 }
+
