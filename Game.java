9e63fff4699133e23b9e3de65100b2b043a58d1d
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2024-11-24 15:57:28 -0700

    set up submitHand method

--- Game.java
+++ Game.java
@@ -3,13 +3,51 @@ import java.util.ArrayList;
 public class Game {
 	private int playerAmount;
 	private ArrayList<Player> players;
-	
+	private int currentPlayerIdx;
+
 	/*
 	 * Constructor for game with no players
 	 */
 	public Game() {
 		this.playerAmount = 0;
 		this.players = new ArrayList<Player>();
+		this.currentPlayerIdx = 0;
+	}
+
+	/*
+	 * Method return amount of players
+	 * 
+	 * @return int rep. amount of players
+	 */
+	public int getPlayerAmount() {
+		return this.playerAmount;
+	}
+
+	/*
+	 * return the current player
+	 * 
+	 */
+	public boolean submitHand() {
+		Player curPlayer;
+		// if we are the last player
+		if (currentPlayerIdx == players.size() - 1) {
+			// get the last player
+			curPlayer = players.get(currentPlayerIdx);
+			// try to make make the last player submitHand
+			boolean result = curPlayer.submitHand();
+			// if not possible
+			if (result == false) {
+				// end of game
+				return false;
+			}
+		}
+		// update current player to next player
+		currentPlayerIdx = ((currentPlayerIdx + 1) % playerAmount);
+		curPlayer = players.get(currentPlayerIdx);
+		// make sure next player restart there roll count to 3 and holds to zero
+		curPlayer.startTurn();
+		// return true that we were able to submit hand
+		return true;
 
 	}
 
