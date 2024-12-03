1cd94ee8572adb620a640479450a8e8cdbbb0745
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-02 21:14:13 -0700

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class CPU extends Player {
 
 	private final CpuMode mode;
@@ -22,11 +25,73 @@ public class CPU extends Player {
 	 * 
 	 * @pre rollCount != 3
 	 * 
-	 * @post best Category will be sumbited
+	 * @post best Category will be submitted
 	 */
 	public Category getBestCategory() {
 		assert this.getRollCount() != 3;
-		
+
+		List<Category> unfilled = this.getUnfilledCategories();
+
+		HashMap<Category, Integer> scores = new HashMap<>();
+
+		for (Category cat : unfilled) {
+			switch(cat) {
+				case ONES:
+					int onesScore = getUpperSection(this.getHand(), 1);
+					scores.put(cat, onesScore);
+					break;
+				case TWOS:
+					int twosScore = getUpperSection(this.getHand(), 2);
+					scores.put(cat, twosScore);
+					break;
+				case THREES:
+					int threesScore = getUpperSection(this.getHand(), 3);
+					scores.put(cat, threesScore);
+					break;
+				case FOURS:
+					int foursScore = getUpperSection(this.getHand(), 4);
+					scores.put(cat, foursScore);
+					break;
+				case FIVES:
+					int fivesScore = getUpperSection(this.getHand(), 5);
+					scores.put(cat, fivesScore);
+					break;
+				case SIXES:
+					int sixesScore = getUpperSection(this.getHand(), 6);
+					scores.put(cat, sixesScore);
+					break;
+				case THREEOFKIND:
+					int threeOfAKindScore = getThreeOfAKind(this.getHand());
+					scores.put(cat, threeOfAKindScore);
+					break;
+				case FOUROFKIND:
+					int fourOfAKindScore = getFourOfAKind(this.getHand());
+					scores.put(cat, fourOfAKindScore);
+				case FULLHOUSE:
+					int fullHouseScore = getFullHouse(this.getHand());
+					scores.put(cat, fullHouseScore);
+					break;
+				case SMALLSTRAIGHT:
+					int smallStraightScore = getSmallStraight(this.getHand());
+					scores.put(cat, smallStraightScore);
+					break;
+				case LARGESTRAIGHT:
+					int largeStraightScore = getLargeStraight(this.getHand());
+					scores.put(cat, largeStraightScore);
+					break;
+				case YAHTZEE:
+					int yahzteeScore = getYahtzee(this.getHand());
+					scores.put(cat, yahzteeScore);
+					break;
+				case CHANCE:
+					int chanceScore = getChance(this.getHand());
+					scores.put(cat, chanceScore);
+					break;
+			}
+		}
+
+
+
 		
 		return null;
 	}
@@ -37,12 +102,12 @@ public class CPU extends Player {
 	 * 
 	 * @pre rollCount != 3
 	 * 
-	 * @post first Category will be sumbited
+	 * @post first Category will be submitted
 	 */
 	public Category getFirstCategory() {
 		assert this.getRollCount() != 3;
 
-
+		
 		return this.getUnfilledCategories().get(0);
 	}
 
