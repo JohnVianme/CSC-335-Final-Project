c70559e35987043aebc42f42399b668a8bb81b50
Garret <134025929+gsw2019@users.noreply.github.com>
2024-12-02 21:23:54 -0700

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -37,58 +37,59 @@ public class CPU extends Player {
 		for (Category cat : unfilled) {
 			switch(cat) {
 				case ONES:
-					int onesScore = getUpperSection(this.getHand(), 1);
+					int onesScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, onesScore);
 					break;
 				case TWOS:
-					int twosScore = getUpperSection(this.getHand(), 2);
+					int twosScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, twosScore);
 					break;
 				case THREES:
-					int threesScore = getUpperSection(this.getHand(), 3);
+					int threesScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, threesScore);
 					break;
 				case FOURS:
-					int foursScore = getUpperSection(this.getHand(), 4);
+					int foursScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, foursScore);
 					break;
 				case FIVES:
-					int fivesScore = getUpperSection(this.getHand(), 5);
+					int fivesScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, fivesScore);
 					break;
 				case SIXES:
-					int sixesScore = getUpperSection(this.getHand(), 6);
+					int sixesScore = CheckRollType.getUpperSection(this.getHand(), cat);
 					scores.put(cat, sixesScore);
 					break;
 				case THREEOFKIND:
-					int threeOfAKindScore = getThreeOfAKind(this.getHand());
+					int threeOfAKindScore = CheckRollType.getThreeOfAKind(this.getHand());
 					scores.put(cat, threeOfAKindScore);
 					break;
 				case FOUROFKIND:
-					int fourOfAKindScore = getFourOfAKind(this.getHand());
+					int fourOfAKindScore = CheckRollType.getFourOfAKind(this.getHand());
 					scores.put(cat, fourOfAKindScore);
 				case FULLHOUSE:
-					int fullHouseScore = getFullHouse(this.getHand());
+					int fullHouseScore = CheckRollType.getFullHouse(this.getHand());
 					scores.put(cat, fullHouseScore);
 					break;
 				case SMALLSTRAIGHT:
-					int smallStraightScore = getSmallStraight(this.getHand());
+					int smallStraightScore = CheckRollType.getSmallStraight(this.getHand());
 					scores.put(cat, smallStraightScore);
 					break;
 				case LARGESTRAIGHT:
-					int largeStraightScore = getLargeStraight(this.getHand());
+					int largeStraightScore = CheckRollType.getLargeStraight(this.getHand());
 					scores.put(cat, largeStraightScore);
 					break;
 				case YAHTZEE:
-					int yahzteeScore = getYahtzee(this.getHand());
+					int yahzteeScore = CheckRollType.getYahtzee(this.getHand());
 					scores.put(cat, yahzteeScore);
 					break;
 				case CHANCE:
-					int chanceScore = getChance(this.getHand());
+					int chanceScore = CheckRollType.getChance(this.getHand());
 					scores.put(cat, chanceScore);
 					break;
 			}
 		}
+
 
 
 
